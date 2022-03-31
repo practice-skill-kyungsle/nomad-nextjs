8b6372429535433aea62c8e20357dd2e736870cf
keinn51 <soogood97@gmail.com>
2022-03-31 16:38:07 +0900

    1.4 CSS Module

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
-export default function Home() {
-  return "NextJS is Framework!";
+import NavBar from "../components/navbar";
+
+export default function Potato() {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import NavBar from "../components/navbar";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
   return (
     <div>
+      <NavBar />
       <h1>Hello</h1>
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
